0c4c1ce9f6541da2509731e0b7ea66a3db40eb31
Elliot Thomas <elliotnthomas@gmail.com>
2016-12-18 17:32:12 -0600

    added display function

--- public/client.js
+++ public/client.js
@@ -4,14 +4,15 @@ $(document).ready (function (){
   console.log("in JQ");
 
   enableButtons();
-});
+});//end doc ready
 
 function enableButtons () {
-  $(document).on('click', '#submitNewTask', getUserTask);
+  $(document).on('click', '#submitNewTask', postUserTask);
+  $(document).on('click', '.delete', deleteTask);
 
-};//end clicks
+};//end enable buttons
 
-function getUserTask (){
+function postUserTask (){
   var newTaskValue = $('#newTask').val();
 
   var objectToSend = {
@@ -30,4 +31,47 @@ $.ajax ({
     console.log('error with ajax call');
   }
 });//end ajax call
-};//end get user task
+getTask();
+};//end post user task
+
+function getTask () {
+  $.ajax ({
+    type: 'GET',
+    url: '/getTask',
+    success: function ( response ) {
+      console.log( 'back from database ', response);
+      displayTask(response);
+    },
+    error: function () {
+      console.log('error with ajax call');
+    }//end error stmt
+  });//end ajax call back from database
+};//end get task
+
+function deleteTask () {
+  var deleteTask = {
+    id: $()
+  }
+  $.ajax ({
+    type: 'PUT',
+    url: '/deleteTask',
+    
+  });//end ajax call
+};//end delete task
+
+function displayTask (array){
+  console.log('in display task', array)
+  var allTasks = '<tr><td>Task</td><td>Completed?</td><td>Complete Task<td>Delete Task</td>'
+  for (var i = 0; i < array.length; i++) {
+    if (array[i].complete == true) {
+    allTasks += '<tr><td>' + array[i].task + '</td><td>&#9989;</td>';
+    allTasks += '<td><button type="button" class = "complete" data ="'+ array[i].id + '">Complete</button>';
+    allTasks += '<td><button type="button" class = "delete" data ="'+ array[i].id  + '">Delete</button>'
+  } else {
+    allTasks += '<tr><td>' + array[i].task + '</td><td>No</td>'
+    allTasks += '<td><button type="button" class = "complete" data ="'+ array[i].id + '">Complete</button>';
+    allTasks += '<td><button type="button" class = "delete" data ="'+ array[i].id  + '">Delete</button>'
+  }
+  $('#outputTable').html( allTasks );
+  }//end for loop
+};//end display task
--- server/app.js
+++ server/app.js
@@ -1,35 +1,56 @@
 var express = require('express')
-var app = express ();
+var app = express();
 var path = require('path');
-var bodyParser = require ('body-parser')
+var bodyParser = require('body-parser')
 var pg = require('pg');
-var urlEncodedParser = bodyParser.urlencoded({ extended:true });
+var urlEncodedParser = bodyParser.urlencoded({
+    extended: true
+});
 var port = process.env.PORT || 8080;
 
 var connectionString = 'postgres://localhost:5432/task-master';
 
-app.listen(port, function ( req, res ){
-  console.log('server is listening on:', port);
-});//end app listen
+app.listen(port, function(req, res) {
+    console.log('server is listening on:', port);
+}); //end app listen
 
-app.get ('/', function( req, res ){
-  console.log('Base URL hit');
-  res.sendFile(path.resolve('public/index.html'));
-});//end app get for base URL
+app.get('/', function(req, res) {
+    console.log('Base URL hit');
+    res.sendFile(path.resolve('public/index.html'));
+}); //end app get for base URL
 
-app.post('/postTask', urlEncodedParser, function( req, res ){
-  console.log('creating new task', req.body);
-  //connecting to the database
-  pg.connect(connectionString, function (err, client, done){
-    if(err){
-      console.log('error in connecting to database');
-    } else{
-      client.query('INSERT INTO tasks (task, complete) values($1, $2)', [req.body.newTask, req.body.complete]);
-    }//end else statment
-  });//end pg connect to database
-});//end post task
+app.get('/getTask', urlEncodedParser, function(req, res) {
+    pg.connect(connectionString, function(err, client, done) {
+        if (err) {
+            console.log('Error in connecting to Database');
+        } else {
+            console.log('connected to database');
+            var query = client.query('SELECT * FROM tasks')
+            var taskArray = [];
+            query.on('row' , function(row) {
+                taskArray.push(row);
+            }); //end query on row
+            query.on('end' , function() {
+                done();
+                console.log('Sending array back to client', taskArray);
+                
+                res.send(taskArray);
+            }); //end query on end
+        } //end else stmt
+    }); //end pg connect
+}); //end get task
 
-app.get ('/')
 
+app.post('/postTask', urlEncodedParser, function(req, res) {
+    console.log('creating new task', req.body);
+    //connecting to the database
+    pg.connect(connectionString, function(err, client, done) {
+        if (err) {
+            console.log('error in connecting to database');
+        } else {
+            client.query('INSERT INTO tasks (task, complete) values($1, $2)', [req.body.newTask, req.body.complete]);
+        } //end else statment
+    }); //end pg connect to database
+}); //end post task
 
 app.use(express.static('public'));
